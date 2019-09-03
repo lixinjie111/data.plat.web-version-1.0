fd1abc7f529b2d1d1010242226773b92e8390ed9
d <we>
2019-09-03 19:41:00 +0800

    登录样式修改

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
-
+    <div class="c-wrapper-20">
         <div class="index-logo-wrap">
             <img class="index-logo" src="static/images/banner.png">
             <p class="index-text">智能网联汽车数据管理平台</p>
         </div>
-
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'Home',
+    name: 'Index',
     data () {
         return {
         }
@@ -21,30 +21,29 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/scss/theme.scss';
-
+.c-wrapper-20 {
     .index-logo-wrap {
+        position: absolute;
+        left: 20px;
+        right: 20px;
+        top: 20px;
+        bottom: 20px;
+        background-color: $black;
+    }
+	.index-logo {
+		width: 100%;
+        height: 100%;
+	}
+    .index-text {
         position: absolute;
         left: 0;
         right: 0;
         top: 0;
         bottom: 0;
-        background-color: $black;
-        .index-logo {
-            width: 100%;
-            height: 100%;
-        }
-        .index-text {
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            @include layoutMode();
-            font-size: 36px;
-            color: #fff;
-            letter-spacing: 5px;
-        }
+        @include layoutMode();
+        font-size: 36px;
+        color: #fff;
+        letter-spacing: 5px;
     }
-	
-
+}
 </style>
