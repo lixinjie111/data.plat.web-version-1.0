58226959a49f18a2027e36d0ae6a7eb9dcb256e0
shilu <shilu@tusvn.comm>
2019-12-03 19:03:34 +0800

    测试地址管理

--- static/config/config.js
+++ static/config/config.js
@@ -17,6 +17,15 @@ window.config = {
     downloadUrl: 'http://120.133.21.14:9091/dataPlatApp/cam/download/', //导出&&下载地址
     websocketUrl:'ws://120.133.21.14:49982/mon',
 
+    //测试环境 内网
+    // staticUrl: "http://10.0.1.181:9090/", //静态文件路径
+    // baseUrl: 'http://10.0.1.181',
+    // monUrl: 'http://10.0.1.183',
+    // url: 'http://10.0.1.181:9091/dataPlatApp/',//数据平台
+    // operateUrl: 'http://10.0.1.181:9090/operateApp/',//运营平台
+    // downloadUrl: 'http://10.0.1.181:9091/dataPlatApp/cam/download/', //导出&&下载地址
+    // websocketUrl:'ws://10.0.1.183:49982/mon',
+
     //上海正式环境  外网
     // staticUrl: "http://116.236.72.204:49090/", //静态文件路径
     // baseUrl:'http://116.236.72.204',
